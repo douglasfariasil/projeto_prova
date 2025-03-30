a41ba488130b2d0ed9fc52e5e22cde9627a062ff
Douglasfariasil <douglasfariasil@outlook.com>
2025-03-30 15:17:36 -0300

    meu primeiro commit

--- escola/main.py
+++ escola/main.py
@@ -1,1 +1,25 @@
+# Aprovação ou reprovação na prova da escola.
+# Lista de alunos e suas respectivas médias
+
+alunos = {
+    "Douglas": 9,
+    "Fernanda": 10,
+    "João": 6,
+    "Mamuela": 8,
+    "Miguel": 8,
+    "Cintia": 9,
+    "Maria": 5,
+    "Regina": 4,
+    "Pedro": 5,
+    "Renata": 3,
+    "Felipe": 2,
+    "Raquel": 3,
+    "Natalhia": 7
+}
+
+# Listas para armazenar os alunos aprovados, em recuperação e reprovados
+
+aprovados = []
+recuperacao = []
+reprovados = []
 
